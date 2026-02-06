ee65a5f4e707c69278c761e9cf60e33f511d7cdc
it3richard <it3@moss.sk>
2026-02-06 10:31:33 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,39 +1,15 @@
 import pandas as pd
-import ibm_db
-import ibm_db_dbi
+import pyodbc
 
-# --- nastavenia DB ---
-DB_HOST = "tvoj_host"       # IP alebo hostname DB
-DB_PORT = "50000"            # port DB2
-DB_NAME = "ESO"              # názov databázy / schémy
+DB_DSN = "MOSSDB"
 DB_USER = "esoro"
 DB_PASSWORD = "esor4moss"
 
 def load_data(sql):
-    """
-    Funkcia pre načítanie dát z DB2 do pandas DataFrame
-    Schéma ESO sa nastaví hneď po pripojení.
-    """
-    # connection string pre IBM DB2
-    conn_str = (
-        f"DATABASE={DB_NAME};"
-        f"HOSTNAME={DB_HOST};"
-        f"PORT={DB_PORT};"
-        f"PROTOCOL=TCPIP;"
-        f"UID={DB_USER};"
-        f"PWD={DB_PASSWORD};"
-    )
-    
-    # pripojenie
-    conn = ibm_db.connect(conn_str, "", "")
-    
-    # nastavenie predvolenej schémy
-    ibm_db.exec_immediate(conn, "SET CURRENT SCHEMA ESO")
-    
-    # pandas DataFrame z SQL query
-    pdf = pd.read_sql(sql, ibm_db_dbi.Connection(conn))
-    
-    # zatvorenie spojenia
-    ibm_db.close(conn)
-    
-    return pdf
+    conn = pyodbc.connect(f"DSN={DB_DSN};UID={DB_USER};PWD={DB_PASSWORD}")
+    cursor = conn.cursor()
+    cursor.execute("SET SCHEMA ESO")  # nastavenie predvolenej schémy
+    conn.commit()
+    df = pd.read_sql(sql, conn)
+    conn.close()
+    return df
